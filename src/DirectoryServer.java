1f821481f574f86ede45cfd9800f6cb456f532c6
jgrgic <jgrgic@ryerson.ca>
2019-03-27 13:07:49 -0400

    sendData and sendToNextServer functions completed

--- src/DirectoryServer.java
+++ src/DirectoryServer.java
@@ -9,7 +9,10 @@ public class DirectoryServer {
 
         //initializes a server with a port, a unique identifier, location of the next port in DHT
         //and the next servers IP
-        int port, serverNum, nextPort;
+        int port = 20310;
+        //user inputs which server number this will be
+        int serverNum = Integer.parseInt(args[0]);
+        int nextPort = 20310;
         String nextIP;
         Server server = new Server(port, serverNum, nextPort, nextIP);
     }
@@ -86,6 +89,7 @@ public class DirectoryServer {
 
                 mainTCPThread = new Thread(runTCP);
                 mainUDPThread = new Thread(runUDP);
+
                 //starts the threads
                 mainTCPThread.start();
                 mainUDPThread.start();
@@ -120,9 +124,13 @@ public class DirectoryServer {
             }
         };
 
-        Runnable runTDP = new Runnable() {
+        Runnable runUDP = new Runnable() {
             public void run() {
+                System.out.println("Starting UDP thread");
 
+                while(true) {
+                    String message;
+                }
             }
         };
 
@@ -130,13 +138,41 @@ public class DirectoryServer {
 
         }
 
-        public void sendToNextServer(String message) {
+        //method to send data to the next available server
+        public void sendToNextServer(String message) throws IOException {
+            //opens a socket on the next server
+            Socket nextServer = new Socket(nextServerIP, nextPort);
 
-        }
+            //creates an OutPutStream for the next server based on passed data
+            OutputStream sentOutData = nextServer.getOutputStream();
 
-        public void sendToClient() {
+            //creates an object to be sent based on the data
+            DataOutputStream outbound = new DataOutputStream(sentOutData);
 
-        }
+            //attaches a message to it
+            outbound.writeUTF(message);
+
+            //closes the server
+            nextServer.close();
+        }//end of sendToNextServer()
+
+        //method to send data to the client via UDP
+        public void sendData(String messageToBeSent, String myIP, int myPort) throws IOException {
+            //byte array to store the data
+            byte[] dataToBeSent = new byte[1024];
+
+            //get the size of the data to be sent in bytes
+            dataToBeSent = messageToBeSent.getBytes();
+
+            //get the ip of the source
+            InetAddress IP = InetAddress.getByName(myIP);
+
+            //create a packet with the obtained information
+            DatagramPacket packetToBeSent = new DatagramPacket(dataToBeSent, dataToBeSent.length, IP, myPort);
+
+            //send the packet out via UDP
+            UDPSocket.send(packetToBeSent);
+        } //end of sendData()
 
         public String[] exit() {
 
@@ -149,26 +185,69 @@ public class DirectoryServer {
 
     public static class CustomUDP {
 
+        final int statusCode200 = 200;
+        final int statusCode400 = 400;
+        final int statusCode404 = 404;
+        final int statusCode505 = 505;
+
+        int customPort;
+        int nextPort;
+        String myIP;
+        String nextIP;
+
+        DatagramSocket UDPSocket;
+        Thread customThread;
+
         public CustomUDP() {
 
         }
 
-        Runnable CustomRunnalbe = new Runnable() {
+        Runnable CustomRunnable = new Runnable() {
             public void run() {
 
             }
         };
 
+        //kills the thread and closes the socket
         public void kill() {
+            customThread.stop();
+            UDPSocket.close();
+        } //end of kill()
 
-        }
+        //method to send data to the next available server
+        public void sendToNextServer(String messageToBeSent) throws IOException {
+            //opens a socket on the next server
+            Socket nextServer = new Socket(nextIP, nextPort);
 
-        public void sendToNextServer() {
+            //creates an OutPutStream for the next server based on the provided data
+            OutputStream sentOutData = nextServer.getOutputStream();
 
-        }
+            //creates the object to be sent based on the data
+            DataOutputStream outbound = new DataOutputStream(sentOutData);
 
-        public void sendToClient() {
+            //attaches a message to it
+            outbound.writeUTF(messageToBeSent);
 
-        }
+            //closes the connection
+            nextServer.close();
+        } //end of sendToNextServer()
+
+        //method to send data to the client via UDP
+        public void sendData(String messageToBeSent, String myIP, int myPort) throws IOException {
+            //byte array to store the data
+            byte[] dataToBeSent = new byte[1024];
+
+            //get the size of the data to be sent in bytes
+            dataToBeSent = messageToBeSent.getBytes();
+
+            //get the ip of the source
+            InetAddress IP = InetAddress.getByName(myIP);
+
+            //create a packet with the obtained information
+            DatagramPacket packetToBeSent = new DatagramPacket(dataToBeSent, dataToBeSent.length, IP, myPort);
+
+            //send the packet out via UDP
+            UDPSocket.send(packetToBeSent);
+        }//end of sendData()
     }
 }
