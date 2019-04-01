9d2d3ec84d31f7a9be56452f61670b1e83827803
jgrgic <jgrgic@ryerson.ca>
2019-04-01 13:52:23 -0400

    Bunch of stuff in PeerServer completed; getTime(), getModifiedTime(), thread is almost done; need to deal with getting the file from the table

--- src/PeerServer.java
+++ src/PeerServer.java
@@ -1,3 +1,6 @@
+import java.nio.charset.Charset;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -14,7 +17,7 @@ public class PeerServer {
     //starting port and arrayList that is maintained across the peer servers
     //follows the same format as directory server
     int startingPort;
-    ServerSocket serverTCPsocket;
+    ServerSocket serverTCPSocket;
     public static ArrayList<CustomTCP> customTCPList = new ArrayList<CustomTCP>();
     Thread serverThread;
 
@@ -22,7 +25,7 @@ public class PeerServer {
     public PeerServer(int port) {
         startingPort = port;
         try {
-            serverTCPsocket = new ServerSocket(startingPort);
+            serverTCPSocket = new ServerSocket(startingPort);
             serverThread = new Thread(TCPRunnable);
             serverThread.start();
         }
@@ -38,7 +41,7 @@ public class PeerServer {
                 String message;
 
                 try {
-                    Socket fromClient = serverTCPsocket.accept();
+                    Socket fromClient = serverTCPSocket.accept();
                     DataInputStream input = new DataInputStream(fromClient.getInputStream());
                     message = input.readUTF();
                     Scanner passedMessage = new Scanner(message);
@@ -58,35 +61,182 @@ public class PeerServer {
             }
 
         }
-    };
+    }; //end of TCPRunnable()
 
+    //finds an open port to connect to
+    //exactly like findUDPPort in DirectoryServer
     public int findPort(){
 
-    }
+        int port = 0;
+        int findPort = startingPort;
+        boolean finished = false;
+
+        while (finished == false) {
+            try {
+                ServerSocket possiblePort = new ServerSocket(findPort);
+                finished = true;
+                possiblePort.close();
+                break;
+            }
+            catch (Exception e) {
+                findPort++;
+            }
+        }
+        port = findPort;
+        return port;
+    }//end of findPort()
 
     public class CustomTCP {
 
-        public CustomTCP(int port) {
+        //declare error codes
+        final int statusCode200 = 200;
+        final int statusCode400 = 400;
+        final int statusCode404 = 404;
+        final int statusCode505 = 505;
 
-        }
+        ServerSocket customTCPSocket;
+        Thread TCPThread;
+
+        //same as the above constructor for PeerServer
+        public CustomTCP(int port) {
+            try {
+                customTCPSocket = new ServerSocket(port);
+                TCPThread = new Thread(TCPRunnable);
+                TCPThread.start();
+            }
+            catch (Exception e) {
+                System.out.println("Unavailable port");
+            }
+        }//end of CustomTCP()
 
         Runnable TCPRunnable = new Runnable() {
             public void run() {
+                try {
+                    //empty strings for all the information required to retrieve the file
+                    byte[] completeArray = null;
+                    String message = "";
+                    String nameOfFile = "";
+                    String request = "";
+                    String version = "";
+                    String response = "";
+                    String connection = "";
+                    String type = "";
+                    String timeString = getTime();
+                    Socket socket = customTCPSocket.accept();
 
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    message = input.readUTF();
+
+                    Scanner scanner = new Scanner(message);
+                    request = scanner.next();
+
+                    if (request.equals("get")) {
+                        nameOfFile = scanner.next();
+                        version = scanner.next();
+                        connection = "Close";
+                        type = "image/jpg";
+
+                        if (version.equals("HTTP/1.1")) {
+                            nameOfFile = nameOfFile.substring(1);
+                            File file = new File(nameOfFile);
+
+                            try {
+                                String newFile1 = "";
+                                String newFile2 = "";
+                                newFile1 = nameOfFile.substring(0, nameOfFile.indexOf(".jpg"));
+                                newFile2 = nameOfFile.substring(0, nameOfFile.indexOf(".jpg"));
+
+                            }
+                            catch (Exception e) {
+                                response = createResponse(statusCode400, timeString, null, null, null, connection, null);
+                                completeArray = response.getBytes(Charset.forName("UTF-8"));
+                            }
+                        }
+                        else {
+                            response = createResponse(statusCode505, timeString, null, null, null, connection, null);
+                            completeArray = response.getBytes(Charset.forName("UTF-8"));
+                        }
+
+                    }
+
+                    OutputStream output = socket.getOutputStream();
+                    DataOutputStream outputStream = new DataOutputStream(output);
+                    outputStream.writeInt(completeArray.length);
+                    outputStream.write(completeArray, 0, completeArray.length);
+                    socket.close();
+                    customTCPSocket.close();
+
+                    for (int i = 0; i < PeerServer.customTCPList.size(); i++) {
+                        if (PeerServer.customTCPList.get(i).equals(this)) {
+                            PeerServer.customTCPList.remove(i);
+                            TCPThread.stop();
+                            break;
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    System.out.println("Something went horribly wrong. Good luck figuring it out chump");
+                }
             }
         };
 
+        //gets the current time as a string
         public String getTime() {
+            Date date = new Date();
+            Scanner input = new Scanner(date.toString());
+            String day = input.next();
+            String month = input.next();
+            String number = input.next();
+            DateFormat format = new SimpleDateFormat("yyyy HH:mm:ss");
+            Date time = new Date();
+            String timeString = day + ", " + number + " " + month + " " + format.format(time) + "GMT";
+            return timeString;
+        }//end of getTime()
 
-        }
-
+        //gets time the file was last modified at
+        //follows the same format as getTime()
         public String getModifiedTime(File file) {
+            Date date = new Date(file.lastModified());
+            Scanner input = new Scanner(date.toString());
+            String day = input.next();
+            String month = input.next();
+            String number = input.next();
+            DateFormat format = new SimpleDateFormat("yyyy HH:mm:ss");
+            Date time = new Date(file.lastModified());
+            String timeString = day + ", " + number + " " + month + " " + format.format(time) + "GMT";
+            return timeString;
+        }//end of getModifiedTime()
 
-        }
+        //creates an HTTP response with appropriate codes and response
+        public String createResponse(int code, String currentDate, String lastModifiedDate, String range, String length, String connection, String type) {
 
-        public String createResponse() {
+            String response = "";
 
-        }
+            if (code == statusCode200) {
+                //creates the appropriate response code with all information
+                response = response + "HTTP/1.1 " + code + "OK\r\n";
+                response = response + "Connection: " + connection + "\r\n";
+                response = response + "Date: " + currentDate + "\r\n";
+                response = response + "Last Modified: " + lastModifiedDate + "\r\n";
+                response = response + "Accepted Range: " + range + "\r\n";
+                response = response + "Content Length: " + length + "\r\n";
+                response = response + "Content Type: " + type + "\r\n\r\n";
+            }
+            else {
+                if (code == statusCode400) {
+                    response = response + "HTTP/1.1 " + code + " " + "Bad request\r\n";
+                }
+                else if (code == statusCode404) {
+                    response = response + "HTTP/1.1 " + code + " " + "Not Found\r\n";
+                }
+                else if (code == statusCode505) {
+                    response = response + "HTTP/1.1 " + code + " " + "HTTP version not supported\r\n";
+                }
+                response = response + "Connection: " + connection + "\r\n";
+                response = response + "Date: " + currentDate + "\r\n";
+            }
+            return response;
+        }//end of createResponse()
 
     }
 }
